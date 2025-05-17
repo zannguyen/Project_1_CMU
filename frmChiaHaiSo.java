e2cb84537b5e56bf48dec13cd6bacaba1ba37bd5
Duy Anh <hoanvu201215@gmail.com>
2025-05-17 07:27:50 +0700

    Commit chia hai so

--- frmChiaHaiSo.java
+++ frmChiaHaiSo.java
@@ -1,3 +1,56 @@
-public class frmChiaHaiSo {
-    
+﻿import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class frmThucHienChia extends JFrame implements ActionListener {
+    private JTextField txtSo1, txtSo2;
+    private JButton btnTinh;
+    private JLabel lblKetQua;
+
+    public frmThucHienChia() {
+        setTitle("Form Thực Hiện Chia");
+        setSize(400, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new GridLayout(4, 2, 10, 10));
+
+        add(new JLabel("Số thứ nhất (a):"));
+        txtSo1 = new JTextField();
+        add(txtSo1);
+
+        add(new JLabel("Số thứ hai (b):"));
+        txtSo2 = new JTextField();
+        add(txtSo2);
+
+        btnTinh = new JButton("Tính");
+        btnTinh.addActionListener(this);
+        add(btnTinh);
+
+        lblKetQua = new JLabel("Kết quả: ");
+        add(lblKetQua);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        try {
+            double a = Double.parseDouble(txtSo1.getText());
+            double b = Double.parseDouble(txtSo2.getText());
+
+            if (b == 0) {
+                lblKetQua.setText("Không thể chia cho 0!");
+            } else {
+                double ketQua = a / b;
+                lblKetQua.setText("Kết quả: " + ketQua);
+            }
+        } catch (NumberFormatException ex) {
+            lblKetQua.setText("Vui lòng nhập số hợp lệ!");
+        }
+    }
+
+    public static void main(String[] args) {
+        new frmThucHienChia();
+    }
 }
